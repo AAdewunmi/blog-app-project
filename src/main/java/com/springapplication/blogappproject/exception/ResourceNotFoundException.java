cc142ffdf05a3b24c959d3a7da76d851a04c3b94
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-05-26 13:02:48 +0100

    Tenth Commit: Implement custom exception class

--- src/main/java/com/springapplication/blogappproject/exception/ResourceNotFoundException.java
+++ src/main/java/com/springapplication/blogappproject/exception/ResourceNotFoundException.java
@@ -1,4 +1,43 @@
 package com.springapplication.blogappproject.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Custom exception to handle resource not found scenarios.
+ * This exception is thrown when a requested resource (like a Post) is not found in the database.
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    private static final long serialVersionUID = 1L;
+    /**
+     * Constructs a new ResourceNotFoundException with the specified resource name, field name, and field value.
+     *
+     * @param resourceName the name of the resource that was not found
+     * @param fieldName    the name of the field that was used to search for the resource
+     * @param fieldValue   the value of the field that was used to search for the resource
+     */
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
 }
