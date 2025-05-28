0fde650dc2cf16fea0c1b23e18fb8ae05a92c17b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-05-28 11:20:55 +0100

    Twelfth Commit: Implement PostDto class

--- src/main/java/com/springapplication/blogappproject/payload/PostDto.java
+++ src/main/java/com/springapplication/blogappproject/payload/PostDto.java
@@ -1,4 +1,12 @@
 package com.springapplication.blogappproject.payload;
 
+import lombok.Data;
+
+@Data
 public class PostDto {
+
+    private  Long id;
+    private String title;
+    private String content;
+    private String description;
 }
