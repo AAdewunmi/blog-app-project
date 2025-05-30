196b4613ca4304e594a3a8d70fdaff091bdc1b7f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-05-30 12:41:54 +0100

    Sixteenth Commit: Implement conversion of DTO to entity

--- src/main/java/com/springapplication/blogappproject/service/impl/PostServiceImpl.java
+++ src/main/java/com/springapplication/blogappproject/service/impl/PostServiceImpl.java
@@ -1,4 +1,27 @@
 package com.springapplication.blogappproject.service.impl;
 
-public class PostServiceImpl {
+import com.springapplication.blogappproject.entity.Post;
+import com.springapplication.blogappproject.payload.PostDto;
+import com.springapplication.blogappproject.repository.PostRepository;
+import com.springapplication.blogappproject.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+        post.setDescription(postDto.getDescription());
+        Post newPost = postRepository.save(post);
+        return null;
+    }
+
 }
