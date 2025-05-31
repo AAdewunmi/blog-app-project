06dcec39092b93fd1dca916f71887b916fd9f664
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-05-31 11:25:16 +0100

    Twenty-First Commit: Implement SecurityFilterChain() method

--- src/main/java/com/springapplication/blogappproject/security/SecurityConfig.java
+++ src/main/java/com/springapplication/blogappproject/security/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.springapplication.blogappproject.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // Explicitly configure CSRF protection
+                .csrf(AbstractHttpConfigurer::disable) // Only disable for non-browser clients like Postman
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
 }
