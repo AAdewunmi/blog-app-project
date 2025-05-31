33b71f73f2583db726333503b4abca49cecc98f9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-05-31 12:35:54 +0100

    Twenty-Four Commit: Implement createPost() method

--- src/main/java/com/springapplication/blogappproject/controller/PostController.java
+++ src/main/java/com/springapplication/blogappproject/controller/PostController.java
@@ -1,7 +1,27 @@
 package com.springapplication.blogappproject.controller;
 
+import com.springapplication.blogappproject.payload.PostDto;
+import com.springapplication.blogappproject.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
 }
