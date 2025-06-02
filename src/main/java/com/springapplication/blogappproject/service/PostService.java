7633fc18296ef8f59eda0b6982f43e84be820fb7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-02 17:01:37 +0100

    Twenty-Five Commit: Declare abstract getAllPosts() abstract method

--- src/main/java/com/springapplication/blogappproject/service/PostService.java
+++ src/main/java/com/springapplication/blogappproject/service/PostService.java
@@ -2,8 +2,11 @@ package com.springapplication.blogappproject.service;
 
 import com.springapplication.blogappproject.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
 
+    List<PostDto> getAllPosts();
 }
