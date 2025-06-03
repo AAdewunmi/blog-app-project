624b384487b6c71d97e5278e02f8f9099a18b8da
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-03 17:16:06 +0100

    Thirty-Six Commit: Refactor getPostById() method

--- src/main/java/com/springapplication/blogappproject/controller/PostController.java
+++ src/main/java/com/springapplication/blogappproject/controller/PostController.java
@@ -5,11 +5,12 @@ import com.springapplication.blogappproject.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Slf4j
 public class PostController {
 
     private PostService postService;
@@ -29,8 +30,17 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
-        return ResponseEntity.ok(postService.getPostById(id));
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        log.info("Receiving request to find post with id: {}", id);
+        try {
+            PostDto post = postService.getPostById(id);
+            log.info("Found post with id: {}", id);
+            return ResponseEntity.ok(post);
+        } catch (Exception e) {
+            log.error("Error finding post with id: {}", id, e);
+            throw e;
+        }
     }
+
 
 }
