758345617319ab5f1294c76a59eed93ada1ddecb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-03 14:25:48 +0100

    Thirty-One Commit: Implement contextLoads() method

--- src/test/java/com/springapplication/blogappproject/BlogAppProjectApplicationTests.java
+++ src/test/java/com/springapplication/blogappproject/BlogAppProjectApplicationTests.java
@@ -1,13 +1,27 @@
 package com.springapplication.blogappproject;
 
+import com.springapplication.blogappproject.controller.PostController;
+import com.springapplication.blogappproject.service.PostService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BlogAppProjectApplicationTests {
 
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostController postController;
+
     @Test
     void contextLoads() {
+        assertNotNull(postService, "PostService should be initialized");
+        assertNotNull(postController, "PostController should be initialized");
     }
+
 
 }
