9667dd4fd7741c1553e3365fe1f437a83b62d286
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-11 14:50:29 +0100

    Seventy-One Commit: Create a skeleton AppConstants class

--- src/main/java/com/springapplication/blogappproject/utils/AppConstants.java
+++ src/main/java/com/springapplication/blogappproject/utils/AppConstants.java
@@ -1,4 +1,9 @@
 package com.springapplication.blogappproject.utils;
-
+/*
+ * AppConstants is a utility class that holds application-wide constants.
+ * It is currently empty but can be used to define constants used throughout the application.
+ */
 public class AppConstants {
+
+
 }
