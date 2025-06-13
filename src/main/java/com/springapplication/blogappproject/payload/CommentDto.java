339dfe929b5fb62d6cc935a3a580dc5b215f3ff2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-13 15:14:59 +0100

    Eighty Commit: Implement CommentDto class

--- src/main/java/com/springapplication/blogappproject/payload/CommentDto.java
+++ src/main/java/com/springapplication/blogappproject/payload/CommentDto.java
@@ -1,10 +1,15 @@
 package com.springapplication.blogappproject.payload;
 
 import lombok.Data;
-
+/*
+ * Data Transfer Object for Comment entity.
+ * This class is used to transfer comment data between layers.
+ */
 @Data
 public class CommentDto {
-
+    /* * Unique identifier for the comment.
+     * This field is used to identify the comment in the database.
+     */
     private long id;
 
     private String name;
