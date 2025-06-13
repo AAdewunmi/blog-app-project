55fb104bfb2d9eeeb8c863e476cd24b1fda32870
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-13 16:03:46 +0100

    Eighty-Two Commit: Declare findByPostId() abstract method

--- src/main/java/com/springapplication/blogappproject/repository/CommentRepository.java
+++ src/main/java/com/springapplication/blogappproject/repository/CommentRepository.java
@@ -3,11 +3,19 @@ package com.springapplication.blogappproject.repository;
 import com.springapplication.blogappproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Repository interface for managing Comment entities.
  * This interface extends JpaRepository to provide CRUD operations for Comment entities.
  */
-public class CommentRepository extends JpaRepository<Comment, Long> {
-
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    /**
+     * Finds all comments associated with a specific post ID.
+     *
+     * @param postId the ID of the post for which comments are to be retrieved
+     * @return a list of comments associated with the specified post ID
+     */
+    List<Comment> findByPostId(Long postId);
 
 }
