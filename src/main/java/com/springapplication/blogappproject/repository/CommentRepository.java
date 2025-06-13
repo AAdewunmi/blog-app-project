ce866e3050f623b2ff0036393b2ca6909dc6af69
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-13 14:44:12 +0100

    Seventy-Eight Commit: Create a skeleton CommentDto class

--- src/main/java/com/springapplication/blogappproject/repository/CommentRepository.java
+++ src/main/java/com/springapplication/blogappproject/repository/CommentRepository.java
@@ -1,4 +1,13 @@
 package com.springapplication.blogappproject.repository;
 
-public class CommentRepository {
+import com.springapplication.blogappproject.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository interface for managing Comment entities.
+ * This interface extends JpaRepository to provide CRUD operations for Comment entities.
+ */
+public class CommentRepository extends JpaRepository<Comment, Long> {
+
+
 }
