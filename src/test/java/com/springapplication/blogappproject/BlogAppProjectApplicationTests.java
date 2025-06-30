eb254a90deb0c388a41dc4496875d6f685df265f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-06-30 14:19:01 +0100

    One-Hundred-Thirty-Five Commit: Implement testModelMapperBean_ReturnsModelMapperInstance() method

--- src/test/java/com/springapplication/blogappproject/BlogAppProjectApplicationTests.java
+++ src/test/java/com/springapplication/blogappproject/BlogAppProjectApplicationTests.java
@@ -4,6 +4,7 @@ import com.springapplication.blogappproject.controller.PostController;
 import com.springapplication.blogappproject.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -40,5 +41,25 @@ class BlogAppProjectApplicationTests {
                 () -> assertNotNull(postService, "PostService should be properly initialized"),
                 () -> assertNotNull(postController, "PostController should be properly initialized")
         );
+    }
+
+    /**
+     * Verifies that the ModelMapper bean is correctly initialized.
+     */
+    @Test
+    @DisplayName("ModelMapper Bean Initialization")
+    void modelMapperBean_IsInitialized() {
+        ModelMapper modelMapper = applicationContext.getBean(ModelMapper.class);
+        assertNotNull(modelMapper, "ModelMapper bean should be properly initialized");
+    }
+    /**
+     * Tests the direct invocation of the modelMapper method in BlogAppProjectApplication.
+     */
+    @Test
+    @DisplayName("Direct ModelMapper Bean Instance Test")
+    void testModelMapperBean_ReturnsModelMapperInstance() {
+        BlogAppProjectApplication application = new BlogAppProjectApplication();
+        ModelMapper modelMapper = application.modelMapper();
+        assertNotNull(modelMapper, "The modelMapper() method should return a non-null instance of ModelMapper");
     }
 }
