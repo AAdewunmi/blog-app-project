73341903dde505d40485153f606d47fd586c6a35
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-02 16:26:15 +0100

    One-Hundred-Forty-Eight Commit: Create a skeleton ErrorDetailsTest class

---
+++ src/test/java/com/springapplication/blogappproject/payload/ErrorDetailsTest.java
@@ -0,0 +1,13 @@
+package com.springapplication.blogappproject.payload;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ErrorDetailsTest {
+
+
+}
