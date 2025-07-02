24602e8dcfc0c746d0622d31c2542c2917f978da
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-02 16:51:29 +0100

    One-Hundred-Fifty-Two Commit: Implement handleBlogAPIException( method

--- src/main/java/com/springapplication/blogappproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springapplication/blogappproject/exception/GlobalExceptionHandler.java
@@ -21,5 +21,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception,
+                                                               WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 }
