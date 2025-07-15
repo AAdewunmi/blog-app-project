e044874a12acad1e1dcb323f2234ce08e31e488d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-15 13:42:10 +0100

    One-Hundred-Ninety-Seven Commit: Declare findByEmail() abstract method

--- src/main/java/com/springapplication/blogappproject/repository/UserRepository.java
+++ src/main/java/com/springapplication/blogappproject/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package com.springapplication.blogappproject.repository;
 
-public interface UserRepository {
+import com.springapplication.blogappproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    //Optional<User> findByUsernameOrEmail(String username, String email);
+
+    //Optional<User> findByUsername(String username);
+
+    //Boolean existsByUsername(String username);
+
+    //Boolean existsByEmail(String email);
+
 }
