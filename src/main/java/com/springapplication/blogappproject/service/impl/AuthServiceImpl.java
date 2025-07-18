01a10eff0ef5fe46dae8a74b2cc43c330bdc9fe1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-18 13:11:20 +0100

    Two-Hundred-Twenty-Seven Commit: Create a skeleton AuthServiceImplTest class

--- src/main/java/com/springapplication/blogappproject/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springapplication/blogappproject/service/impl/AuthServiceImpl.java
@@ -1,4 +1,59 @@
 package com.springapplication.blogappproject.service.impl;
 
-public class AuthServiceImpl {
+import com.springapplication.blogappproject.payload.LoginDto;
+import com.springapplication.blogappproject.service.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation of the AuthService interface for user authentication.
+ * This service provides functionality for authenticating users
+ * and returning a JWT token upon successful login.
+ */
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    /**
+     * Manages user authentication within the system.
+     * This variable is used to authenticate user credentials
+     * and establish security contexts for further operations.
+     */
+    private AuthenticationManager authenticationManager;
+
+    /**
+     * Constructs a new AuthServiceImpl with the provided AuthenticationManager.
+     *
+     * @param authenticationManager the AuthenticationManager used for authenticating users
+     */
+    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    /**
+     * Authenticates a user based on the provided login credentials
+     * (username or email and password). If the authentication is
+     * successful, it returns a JWT token.
+     *
+     * @param loginDto the data transfer object containing the user's
+     *                 login credentials, including username or email
+     *                 and password.
+     * @return a JWT token as a string if the authentication is
+     *         successful.
+     */
+    @Override
+    public String login(LoginDto loginDto) {
+        Authentication authentication =
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsernameOrEmail(),
+                        loginDto.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "User Logged-In Successfully!";
+    }
+
 }
---
+++ src/test/java/com/springapplication/blogappproject/service/impl/AuthServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.springapplication.blogappproject.service.impl;
+
+import com.springapplication.blogappproject.payload.LoginDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+@SpringBootTest
+public class AuthServiceImplTest {
+
+
+}
