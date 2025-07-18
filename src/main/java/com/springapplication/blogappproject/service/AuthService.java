da8bfe9e927e4fc14e847cb9ed44af4905beb651
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-18 12:49:09 +0100

    Two-Hundred-Twenty-Five Commit: Declare login() abstract method

--- src/main/java/com/springapplication/blogappproject/service/AuthService.java
+++ src/main/java/com/springapplication/blogappproject/service/AuthService.java
@@ -1,4 +1,21 @@
 package com.springapplication.blogappproject.service;
 
-public class AuthService {
+import com.springapplication.blogappproject.payload.LoginDto;
+
+/**
+ * Service interface for managing user authentication.
+ * Provides methods related to authentication functionality such as logging in.
+ */
+public interface AuthService  {
+
+    /**
+     * Authenticates a user using the provided login credentials.
+     * Accepts a LoginDto object containing the username or email and password,
+     * and returns a JWT token upon successful authentication.
+     *
+     * @param loginDto the data transfer object containing user credentials for login
+     * @return a string representing the JWT token if authentication is successful
+     */
+    String login(LoginDto loginDto);
+
 }
