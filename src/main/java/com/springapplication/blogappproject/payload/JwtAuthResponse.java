c9fe29b477a1b06f29db7fc3c934d0730422e8e3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-25 11:51:12 +0100

    Two-Hundred-Sixty-Five Commit: Implement JwtAuthResponse class

--- src/main/java/com/springapplication/blogappproject/payload/JwtAuthResponse.java
+++ src/main/java/com/springapplication/blogappproject/payload/JwtAuthResponse.java
@@ -1,4 +1,32 @@
 package com.springapplication.blogappproject.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * JwtAuthResponse is a representation of the authentication response
+ * returned after a successful login or token generation.
+ * It contains the access token and its type, typically used
+ * for securing later API requests.
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtAuthResponse {
+    /**
+     * Represents the access token generated after successful authentication.
+     * This token is used for securing later API requests and ensuring
+     * that they are performed by an authenticated user.
+     */
+    private String accessToken;
+    /**
+     * Represents the type of the token used in authentication responses.
+     * This field is typically set to represent a specific token type,
+     * such as "Bearer", which is commonly used in authorization headers
+     * to indicate the type of token being passed.
+     */
+    private String tokenType = "Bearer";
 }
