20db03c7fac40f4a65e515867c19f3a56aa57167
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-07-28 11:32:36 +0100

    Two-Hundred-Seventy-Seven Commit: Declare getAllCategories() abstract method

--- src/main/java/com/springapplication/blogappproject/service/CategoryService.java
+++ src/main/java/com/springapplication/blogappproject/service/CategoryService.java
@@ -2,10 +2,14 @@ package com.springapplication.blogappproject.service;
 
 import com.springapplication.blogappproject.payload.CategoryDto;
 
+import java.util.List;
+
 public interface CategoryService {
 
     CategoryDto addCategory(CategoryDto categoryDto);
 
     CategoryDto getCategory(Long categoryId);
+
+    List<CategoryDto> getAllCategories();
 
 }
